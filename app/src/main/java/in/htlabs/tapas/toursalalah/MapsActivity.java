aa50f517ca9985a18752b5a04800fe89e317cfb0
htaaps <htaaps.com@gmail.com>
2015-05-30 17:22:24 +0530

    created maps

--- app/src/main/java/in/htlabs/tapas/toursalalah/MapsActivity.java
+++ app/src/main/java/in/htlabs/tapas/toursalalah/MapsActivity.java
@@ -1,20 +1,22 @@
 package in.htlabs.tapas.toursalalah;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ListView;
+import android.widget.Toast;
 
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.apache.http.NameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,133 +24,31 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-import in.htlabs.tapas.toursalalah.adapter.CustomListAdapter;
-import in.htlabs.tapas.toursalalah.app.AppController;
 import in.htlabs.tapas.toursalalah.model.Hotel;
 import in.htlabs.tapas.toursalalah.model.Restaurant;
 import in.htlabs.tapas.toursalalah.model.TPlace;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-    // Log tag
-    private static final String TAG = MapsActivity.class.getSimpleName();
-    // Movies json url
-    private static final String url = "http://www.htlabs.in/student/salalahguide/alldetails.php";
-    private ProgressDialog pDialog;
     private List<Hotel> hotelList = new ArrayList<Hotel>();
     private List<TPlace> placeList = new ArrayList<TPlace>();
     private List<Restaurant> restaurantList = new ArrayList<Restaurant>();
+    private  Marker mPlace[],mRes[],mHotel[];
+    private static final double SLAT=17.0197;
+    private static final double SLON=54.0897;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-        getAllDetails();
-
-        setUpMapIfNeeded();
-
-    }
-
-    public void getAllDetails(){
-
-        pDialog = new ProgressDialog(this);
-        // Showing progress dialog before making http request
-        pDialog.setMessage("Loading...");
-        pDialog.show();
-        // Creating volley request obj
-        JsonArrayRequest prodReq = new JsonArrayRequest(url,new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                Log.d(TAG, response.toString());
-                hidePDialog();
-
-                // Parsing json
-                for (int i = 0; i < response.length(); i++) {
-                    try {
-
-                        JSONObject obj = response.getJSONObject(i);
-                        JSONArray pArray = obj.getJSONArray("posts");
-                        JSONArray pArray_place = obj.getJSONArray("posts_place");
-                        JSONArray pArray_res = obj.getJSONArray("posts_res");
-
-                        for (int j = 0; j < pArray.length(); j++) {
-
-                            JSONObject pro = pArray.getJSONObject(j);
-                            Hotel item = new Hotel();
-                            item.setHName(pro.getString("h_name"));
-                            item.setHImageUrl(pro.getString("h_img"));
-                            item.setHDetails(pro.getString("h_details"));
-                            item.setHId(pro.getString("h_id"));
-                            item.setHLat(pro.getString("h_lat"));
-                            item.setHLon(pro.getString("h_lon"));
-                            hotelList.add(item);
-                        }
-
-                        for (int j = 0; j < pArray_place.length(); j++) {
-
-                            JSONObject pro = pArray_place.getJSONObject(j);
-                            TPlace place_item = new TPlace();
-                            place_item.setPName(pro.getString("p_name"));
-                            place_item.setPImageUrl(pro.getString("p_img"));
-                            place_item.setPDetails(pro.getString("p_details"));
-                            place_item.setPId(pro.getString("p_id"));
-                            place_item.setPLat(pro.getString("p_lat"));
-                            place_item.setPLon(pro.getString("p_lon"));
-                            placeList.add(place_item);
-                        }
-
-                        for (int j = 0; j < pArray_res.length(); j++) {
-
-                            JSONObject pro = pArray_res.getJSONObject(j);
-                            Restaurant res_item = new Restaurant();
-                            res_item.setRName(pro.getString("r_name"));
-                            res_item.setRImageUrl(pro.getString("r_img"));
-                            res_item.setRDetails(pro.getString("r_details"));
-                            res_item.setRId(pro.getString("r_id"));
-                            res_item.setRLat(pro.getString("r_lat"));
-                            res_item.setRLon(pro.getString("r_lon"));
-                            restaurantList.add(res_item);
-                        }
-
-                        // adding movie to movies array
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(TAG, "Error: " + error.getMessage());
-                hidePDialog();
-            }
-        });
-
-        // Adding request to request queue
-        AppController.getInstance().addToRequestQueue(prodReq);
-    }
-
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        hidePDialog();
-    }
-
-    private void hidePDialog() {
-        if (pDialog != null) {
-            pDialog.dismiss();
-            pDialog = null;
-        }
+        new GetAllLocations().execute();
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
+    public boolean onMarkerClick(Marker marker) {
+        return false;
     }
 
     private void setUpMapIfNeeded() {
@@ -165,6 +65,153 @@ public class MapsActivity extends FragmentActivity {
     }
 
     private void setUpMap() {
-         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+        Double lat,lon;
+        mHotel=new Marker[hotelList.size()];
+        mPlace=new Marker[placeList.size()];
+        mRes=new Marker[restaurantList.size()];
+
+        mMap.setOnMarkerClickListener(this);
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        LatLng salalah = new LatLng(SLAT,SLON);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(salalah, 8));
+
+
+        for(int j=0;j<hotelList.size();j++){
+            lat=Double.parseDouble(hotelList.get(j).getHLat());
+            lon=Double.parseDouble(hotelList.get(j).getHLon());
+            mHotel[j]=mMap.addMarker(new MarkerOptions().position(new LatLng(lat,lon)).title(hotelList.get(j).getHName())
+                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        }
+        for(int j=0;j<placeList.size();j++){
+            lat=Double.parseDouble(placeList.get(j).getPLat());
+            lon=Double.parseDouble(placeList.get(j).getPLon());
+            mPlace[j]=mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lon)).title(placeList.get(j).getPName())
+                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        }
+        for(int j=0;j<restaurantList.size();j++){
+            lat=Double.parseDouble(restaurantList.get(j).getRLat());
+            lon=Double.parseDouble(restaurantList.get(j).getRLon());
+            mRes[j]=mMap.addMarker(new MarkerOptions().position(new LatLng(lat,lon)).title(restaurantList.get(j).getRName())
+                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        }
+
+    }
+
+    class GetAllLocations extends AsyncTask<String, String, String> {
+
+        // Movies json url
+        private static final String LOC_URL = "http://www.htlabs.in/student/salalahguide/allloc.php";
+
+        private ProgressDialog pDialog;
+
+        JSONParser jsonParser = new JSONParser();
+
+        private static final String TAG_SUCCESS = "success";
+        private static final String TAG_MESSAGE = "message";
+        private static final String TAG_POSTS = "posts";
+        private static final String TAG_POSTS_PLACE = "posts_place";
+        private static final String TAG_POSTS_RES = "posts_res";
+
+        JSONArray posts=null;
+        JSONArray posts_place=null;
+        JSONArray posts_res=null;
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         */
+        boolean failure = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MapsActivity.this);
+            pDialog.setMessage("Getting all the details...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+            // Check for success tag
+            int success;
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+                Log.d("request!", "starting");
+                // getting product details by making HTTP request
+                JSONObject json = jsonParser.makeHttpRequest(LOC_URL, "POST", params);
+
+                // check your log for json response
+                Log.d("Login attempt", json.toString());
+
+                posts=json.getJSONArray(TAG_POSTS);
+                posts_place=json.getJSONArray(TAG_POSTS_PLACE);
+                posts_res=json.getJSONArray(TAG_POSTS_RES);
+
+                // json success tag
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("Login Successful!", json.toString());
+                    for(int j=0;j< posts.length();j++){
+                        JSONObject obj=posts.getJSONObject(j);
+                        Hotel h=new Hotel();
+                        h.setHId(obj.getString("h_id"));
+                        h.setHName(obj.getString("h_name"));
+                        h.setHLat(obj.getString("h_lat"));
+                        h.setHLon(obj.getString("h_lon"));
+
+                        hotelList.add(h);
+                    }
+                    for(int j=0;j< posts_place.length();j++){
+                        JSONObject obj=posts_place.getJSONObject(j);
+                        TPlace p=new TPlace();
+                        p.setPId(obj.getString("p_id"));
+                        p.setPName(obj.getString("p_name"));
+                        p.setPLat(obj.getString("p_lat"));
+                        p.setPLon(obj.getString("p_lon"));
+
+                        placeList.add(p);
+                    }
+                    for(int j=0;j< posts_res.length();j++){
+                        JSONObject obj=posts_res.getJSONObject(j);
+                        Restaurant r=new Restaurant();
+                        r.setRId(obj.getString("r_id"));
+                        r.setRName(obj.getString("r_name"));
+                        r.setRLat(obj.getString("r_lat"));
+                        r.setRLon(obj.getString("r_lon"));
+
+                        restaurantList.add(r);
+                    }
+
+                        return json.getString(TAG_MESSAGE);
+                } else {
+                    Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+                    return json.getString(TAG_MESSAGE);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * *
+         */
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null) {
+                Toast.makeText(MapsActivity.this, file_url, Toast.LENGTH_LONG).show();
+            }
+            setUpMapIfNeeded();
+        }
+
     }
 }
